d850169ea9d99dc2b7a458ab872a8ebe02169fc2
Online-abayss <83463300+Online-abayss@users.noreply.github.com>
2021-11-25 23:29:21 +0900

    Add files via upload

--- backjoon_step/step_6.py
+++ backjoon_step/step_6.py
@@ -7,23 +7,35 @@
 # 4673
 
 
-def d(n):
-    sum = 0
-    count = []
-    for i in range(len(str(n))):
-        sum += n//(10**i)
-        
-    sum += n%(10**i)
+# # def d(n):
+# for n in range(1,30):
+#     test = list(str(n))
+#     for i in range(0,len(test)):
+#         n += int(test[i])
+#     print(n)
+    
+#     # return(result)
 
-    if n == sum:
-        count = n
+# def d(n):
+#     test = list(str(n))
+#     for i in range(0,len(test)):
+#         n += int(test[i])
 
-    return(count)
+#     return(n)
 
-for i in range(1,30):
-    print(d(i))
-
+# count =[]
+# for i in range(1,10000):
+#     # for j in range(1,10000): # 이게 필요없는 이유 : 문제상 d(75)= 75 + 7 + 5 즉 무조건 list 추가된 값은 n값보다 크기떄문
+#     count.append(d(i))
+#     if i not in count:
+#         print(i)
 
          
-# for i in range(10,20):
 
+# 1065
+n = input()
+test = list(map(int,n))
+if n <100:
+    print(n)
+elif test[0]-test[1] == test[1]-test[2] :
+    print(n)
