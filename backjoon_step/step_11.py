1653a81f12e6dab79a2f41cf1b5f539b3d384a0d
Online-abayss <83463300+Online-abayss@users.noreply.github.com>
2021-12-15 23:20:24 +0900

    Add files via upload

--- backjoon_step/step_11.py
+++ backjoon_step/step_11.py
@@ -25,24 +25,57 @@
 # 인터넷 보기 전의 나의 머리속에서 최대치인듯.
 
 
-n, m = input().split()
-cards = list(map(int,input().split()))
-sum = []
+# n, m = input().split()
+# cards = list(map(int,input().split()))
+# sum = []
 
-for i in range(0,int(n)-2):
-    for j in range(i+1,int(n)-1):
-        for z in range(j+1,int(n)):
-                sum.append(cards[i]+cards[j]+cards[z])
-a = set(sum)
-b = list(a)
-sum = b
-sum.sort()
+# for i in range(0,int(n)-2):
+#     for j in range(i+1,int(n)-1):
+#         for z in range(j+1,int(n)):
+#                 sum.append(cards[i]+cards[j]+cards[z])
+# a1 = set(sum)
+# b = list(a1)
+# sum = b
+# sum.sort()
 
-for a in range(len(sum)):
-    if sum[a]>=int(m):
-        if sum[a]-int(m) < int(m)-sum[a-1]:
-            print(sum[a-1])
-            break
-        print(sum[a])
-        break          
+# for a in range(len(sum)):
+#     if sum[a]>=int(m):
+#         if sum[a]-int(m) < int(m)-sum[a-1]:
+#             print(sum[a-1])
+#             break
+#         print(sum[a])
+#         break
+    
+# print(a)
+# print(sum[a-1])
+# print(sum)
+# 위에는 인터넷 안보고 내 스스로 풀려 했으나 가장 가까운 수를 정하는 방식을 제대로 표현 못하여 실패... 2시간 넘게 고민했는데...
+# 인터넷 보고 감탄.
+
+# n, m = map(int,input().split())
+# cards = list(map(int,input().split()))
+# result = 0
+# for i in range(n):
+#     for j in range(i+1,n):
+#         for k in range(j+1,n):
+#             sum = cards[i]+cards[j]+cards[k]
+#             if sum <= m:
+#                 result = max(sum, result)
+# print(result)
+
+# 내꺼랑 인터넷꺼의 비교점. 1. 내가 2시간동안 머리 싸매고 길게 쓴 코드가 결국엔 단순한 max의 함수 사용...
+# 2. 내껀 리스트에 저장하여 그 모든걸 구한 다음, 중복값을 삭제하여 나열 한뒤에 비교하는거지만, 이건 실시간으로 그때 비교하여 구함.
+# 결론 . 잘 알아두고... 머리가 유연해지기...
+# 글구 모듈 불러와서 함수 사용하는 법들도 있지만. 일단 한바퀴 돌기전에 최대한 사용하지 않을 예정
+
+sum = 0
+test = int(input())
+# creat_num = 198
+for i in range(1,test):
+    if test <10 :
+        print(test)
+        break
+    lst_num = list(str(i))
+    for j in range(len(lst_num)):
+        sum += int(lst_num[j])
 print(sum)
