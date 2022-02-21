45d90557e4bb86d54750314eff5f6df30b2c22d7
Online-abayss <83463300+Online-abayss@users.noreply.github.com>
2022-02-21 17:27:15 +0900

    Add files via upload

--- py/chapter8.py
+++ py/chapter8.py
@@ -81,3 +81,65 @@
 # print(math.ceil(day))
 
 
+## 10250 
+
+
+# # 왜 틀린지 모르겠다. 일단 원하는 값은 다 나온다. // 찾았다... room_list = [] 를 for문 안으로 집어 넣어야 했다. , list를 리셋 해줘야 했었나 보다.
+# case = int(input())
+
+
+# for o in range(case):
+#     room_list = []
+#     H, W, N = list(map(int,input().split()))
+
+#     for i in range(1,W+1):
+
+#         for j in range(1,H+1):
+#             room_num = 100*j+i ## 여길 str(h) + str(w).zfill(2) 이라는 zfiil 함수 이용하는 방법도 있다. zfill이란 str의 문자를 전체 문자열길이()에 맞추기 위해 필요시 앞에 0이 붙는다. 
+#             room_list.append(room_num)
+#     print(int(room_list[N-1]))
+
+
+##  2775
+
+# 0층의 i호수는 i 만큼 산다.
+# k층의 1호수는 1명만 산다.
+# k층의 n 호수는 k-1층의 n 호수의 합만큼 산다. / k층의 n = k-1층의 n의 합.
+
+#인터넷 방법.
+# T = int(input())
+# for i in range(T):
+#     floor = int(input())
+#     room = int(input())
+#     people = [j for j in range(1,room+1)] ## 이걸 풀이하면 j값을 리스트 한개씩 넣는 형식.
+#     for k in range(floor):
+#         for m in range(room-1):
+#             people[m+1] += people[m] # k가 0일시 그냥 j입력된 리스트 호출, 아닐시 덮어쓰기처럼 
+#     print(people[-1]) ## 리스트 맨 마지막을 불러오기.
+# print(people)
+
+## 2839
+
+
+# 너무 복잡해져서 인터넷 힘을 빌림.)
+# n = int(input())
+# f = n//5
+# n %=5
+# t = 0
+
+# while f >=0:
+#     if n%3 ==0:
+#         t = n//3
+#         n = n%3
+#         break
+#     f-=1        
+#     n+=5
+# print((n==0) and (f+t)or -1) ## if를 쓴것처럼 표현 가능.
+
+## 10757
+
+# A,B = map(int,input().split())
+
+# print(A+B)
+
+
---
+++ py/chapter9.py
@@ -0,0 +1,154 @@
+## 1978
+
+## 뭐가 틀린건지 모르겠다.
+# n = int(input())
+
+# test = list(map(int,input().split()))
+
+# num = []
+# for i in test:
+    
+#     cnt = 0
+#     for j in range(1,test[-1]-1):
+#         if i % j == 0:
+#             cnt +=1
+#     if cnt ==2:
+#         num.append(i)
+
+# print(len(num))
+
+
+#인터넷 방법
+# n = int(input())
+
+# test = list(map(int,input().split()))
+# cnt = 0
+# for i in test:
+#     if i > 1:
+#         for j in range(2, i):
+#             if i % j == 0:
+#                 break
+
+#         else:
+#             cnt += 1
+
+# print(cnt)
+
+
+## 2581
+
+# M = int(input())
+# N = int(input())
+# num_list = []
+# for i in range(M,N+1):
+#     if i>1:
+#         for j in range(2,i):
+#             if i % j == 0:
+#                 break
+#         else:
+#             num_list.append(i)
+# if sum(num_list)>0:
+#     print(sum(num_list))
+#     print(num_list[0])
+# else:
+#     print("-1")
+
+
+## 11653
+
+
+## 인터넷 방법.
+# for문과 while문을 어찌 줄여야 하는지 너무 고민이었다.. 역시 인터넷짱.
+# num = int(input())
+
+# if num == 1:
+#     print(' ')
+
+# for i in range(2,num+1):
+#     if num%i == 0:
+#         while num%i ==0:
+#             print(i)
+#             num = num//i
+
+## 1929
+
+
+## 인터넷 방법... 시간초과를 줄일 방법을 못함.
+# m,n = list(map(int,input().split()))
+
+# check =  [1 for i in range(n+1)]
+# check[0] == 0 , check[1] == 0
+
+
+# for i in range(2,int(n**0.5)+1):
+#     for j in range(i+i,n+1,i):
+#         check[j] = 0
+
+# for i in range(m,n+1):
+#     if check[i]:
+#         print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## 1085
+
+# x,y,w,h = map(int,input().split())
+# print(min([x,y,w-x,h-y]))
+
+
+## 3009
+
+## 머리가 안돌아가서 인터넷으로 규칙을 봄.
+
+# X=[]
+# Y=[]
+# new = []
+# for _ in range(3):
+#     x, y = map(int,input().split())
+#     X.append(x)
+#     Y.append(y)
+# for i in X:
+#     if X.count(i)==1:
+#         new.append(i)
+# for j in Y:
+#     if Y.count(j)==1:
+#         new.append(j)
+# print(new[0],new[1])
+
+## 인터넷 방법
+# X=[]
+# Y=[]
+# new = []
+# for _ in range(3):
+#     x, y = map(int,input().split())
+#     X.append(x)
+#     Y.append(y)
+
+# print(min(X,key=X.count),min(Y,key=Y.count))
+
+## 4153
+
+# while(1):
+#     x,y,z = map(int,input().split())
+#     num_list = [x,y,z]
+#     test = sorted(num_list,reverse=True)
+#     if z == y== x ==0:
+#         break
+#     elif test[0]**2 == (test[1]**2) + (test[2]**2):
+#         print("right")
+#     else:
+#         print("wrong")
+
+## 3053
+
