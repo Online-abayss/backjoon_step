44fdac8bd5e0f259bf29c795f652b6e1054ef13e
Online-abayss <83463300+Online-abayss@users.noreply.github.com>
2022-03-13 20:05:13 +0900

    Add files via upload

--- py/algorithm basic/basic(303).py
+++ py/algorithm basic/basic(303).py
@@ -1,14 +1,39 @@
 ## 11005
 
-system = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" #10진법이면 9 까지, 36진법이면 Z까지 표현된다
-N, B = map(int, input().split())
-answer = ''
+# system = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+# N, B = map(int, input().split())
+# answer = ''
 
-while N != 0:
-    answer += str(system[N % B]) #위치로 진법 변환
-    N //= B
+# while N != 0:
+#     answer += str(system[N % B]) 
+#     N //= B
     
-print(answer[::-1])
+# print(answer[::-1])
 
 ## 2745
+
+# a, b = input().rstrip().split()
+# print(int(a, int(b)))
+
+## 11576
+## 인터넷 봄.
+
+# x, y = map(int, input().split())
+# z = int(input())
+# a = list(map(int, input().split()))
+# ten = 0
+# answer =[]
+# for i in range(z):
+#     ten += a[-1] * (x**i)
+#     a.pop(-1)
+# while ten !=0:
+#     answer.append(str(ten % y))
+#     ten = ten // y
+
+# print(' '.join(answer[::-1]))
+
+
+
+
+
 
